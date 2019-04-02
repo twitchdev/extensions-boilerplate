04a65e647d00982104235283eeeeed8fb12ae53e
Leadbetter <llleadbe@88e9fe5d5262.ant.amazon.com>
2019-04-02 09:52:09 -0700

    fixing jest/webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,6 @@ module.exports = (_env,argv)=> {
           test: /\.(js|jsx)$/,
           exclude: /(node_modules|bower_components)/,
           loader: 'babel-loader',
-          options: { presets: ['env', 'react'] }
         },
         {
           test: /\.css$/,
